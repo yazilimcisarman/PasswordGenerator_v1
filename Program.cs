7018c80640ac0e4bef53b5cb3e6ae80c041231d9
Muhammet Seyda Armağan <muhammedseyda@hotmail.com>
2024-04-23 22:27:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAsswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertypüasdfghjklizxcvbnmö*-+";
+            int length = 12;
+            string password = "";
+            for (int i = 0; i < length; i++) 
+            {
+                int randomchars = random.Next(chars.Length);
+                password += chars[randomchars];
+
+            }
+
+            Console.WriteLine(password);
+            Console.ReadLine();
+
+        }
+    }
+}
